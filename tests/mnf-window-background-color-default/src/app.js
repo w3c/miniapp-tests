e24e9e80413f28c4758e3bca5f99e0ed51d6bb36
Martin Alvarez-Espinar <martin.alvarez.espinar@huawei.com>
2022-07-12 11:46:34 +0200

    New test for manifest

---
+++ tests/mnf-window-background-color-default/src/app.js
@@ -0,0 +1,7 @@
+/**
+ * app.js 
+ * This script contains the basic service logic of the MiniApp and 
+ * includes the essential configuration and control of the MiniApp 
+ * lifecycle, including the management of events for launching, 
+ * showing, and hiding the MiniApp.
+ */
---
+++ tests/mnf-window-background-color-default/src/pages/home.js
@@ -0,0 +1,6 @@
+/**
+ * home.js 
+ * This script contains the basic service logic of the 'home' page
+ * of the MiniApp. It includes the essential configuration and control of the page 
+ * lifecycle.
+ */
